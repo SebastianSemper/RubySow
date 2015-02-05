0f2d05a4df01465f49557fc06c767aa3a6d33e4a
Sebastian Semper <seb.semper@gmail.com>
2015-02-05 16:38:33 +0100

    added licence text, more docs, v1.0beta

--- src/inc/commands.rb
+++ src/inc/commands.rb
@@ -1,3 +1,17 @@
+#Copyright (C) 2015  Sebastian Semper
+
+#This program is free software: you can redistribute it and/or modify it under
+#the terms of the GNU General Public License as published by the Free Software
+#Foundation, either version 3 of the License, or (at your option) any later
+#version.
+
+#This program is distributed in the hope that it will be useful, but WITHOUT ANY
+#WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+#PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License along with
+#this program.  If not, see <http://www.gnu.org/licenses/>
+
 # provide name and value
 def setVariable(argv)
     varName = argv[0]
@@ -212,7 +226,6 @@ end
 #writen directly to the target
 def patchIn(argv)
     name = argv[0]
-    puts(Dir.getwd())
     puts("Including #{name}.")
     return parseFile(name)
 end
